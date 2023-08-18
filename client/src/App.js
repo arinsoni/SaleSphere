2cdeb67ffe0b5d1529cd12eb6b39f6ddccfa232d
arinsoni <arinsoni.iitb@gmail.com>
2023-08-18 21:00:35 +0530

    first commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const App = () => <div>Hello from ashraf kabir</div>;
+const App = () => <div>Heya I am Arin Soni</div>;
 
 export default App;
--- client/src/core/Copyright.js
+++ client/src/core/Copyright.js
@@ -9,7 +9,7 @@ export default function Copyright() {
       <Typography variant='body2' color='textSecondary' align='center'>
         {'Copyright © '}
         <Link color='inherit' href='#'>
-          Ashraf Kabir
+          Arin Soni the legend
         </Link>{' '}
         {new Date().getFullYear()}
         {'.'}
